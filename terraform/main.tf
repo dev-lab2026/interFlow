6ae5dd999f3a27098ee1a5bddd4f5c142b6f7fb1
dev-lab2026 <tarek.ferjani@consort-group.com>
2026-08-17 11:31:05 +0200

    Change VM size from Standard_D2s_v5 to Standard_D2a_v4

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_D2s_v5"
+  size                = "Standard_D2a_v4"
   admin_username      = var.admin_username
 
   disable_password_authentication = true
