5650d725ed0e5c81fa7498ccae8dfaad3447f374
dev-lab2026 <tarek.ferjani@consort-group.com>
2026-08-19 10:40:12 +0200

    Update Azure backend configuration for Terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfsstate"
-    storage_account_name = "stinterflowtfsstate"
-    container_name       = "tfsstate"
-    key                  = "interflow.tfsstate"
+    storage_account_name = "stinterflowtfstate01"
+    container_name       = "tfstate"
+    key                  = "interflow.tfstate"
   }
 }
