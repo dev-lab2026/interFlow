7cb37d601aed78f2ff95c4c23be75e7da6a15155
dev-lab2026 <tarek.ferjani@consort-group.com>
2026-08-21 10:43:34 +0200

    Refactor resource group references to data source
    Updated resource group references to use data sources instead of direct resource references. This change allows for better management of the resource group outside of Terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,41 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-interflow"
-  location = var.location
+############################################
+# Resource Group (managed outside Terraform)
+############################################
+
+data "azurerm_resource_group" "rg" {
+  name = "rg-interflow"
 }
+
+############################################
+# Virtual Network
+############################################
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-interflow"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+############################################
+# Subnet
+############################################
 
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+############################################
+# Network Security Group
+############################################
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "interflow-nsg"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "SSH"
@@ -59,17 +74,25 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+############################################
+# Public IP
+############################################
+
 resource "azurerm_public_ip" "publicip" {
   name                = "interflow-ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
 
+############################################
+# Network Interface
+############################################
+
 resource "azurerm_network_interface" "nic" {
   name                = "interflow-nic"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -79,15 +102,23 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+############################################
+# NIC → NSG Association
+############################################
+
 resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+############################################
+# Virtual Machine
+############################################
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   size                = "Standard_D2s_v5"
   admin_username      = var.admin_username
 
@@ -113,4 +144,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "server"
     version   = "latest"
   }
+}
+
+############################################
+# Outputs
+############################################
+
+output "public_ip" {
+  value = azurerm_public_ip.publicip.ip_address
 }
