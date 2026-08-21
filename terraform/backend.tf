a0034f97ab22a829cc1bb9e69bab496500065298
dev-lab2026 <tarek.ferjani@consort-group.com>
2026-08-21 09:53:12 +0200

    Fix formatting in backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "stinterflowtfstate2026"
-    container_name       = "tfstate"
-    key                  = "interflow.tfsstate"
+    resource_group_name   = "rg-tfstate"
+    storage_account_name  = "stinterflowtfstate2026"
+    container_name        = "tfstate"
+    key                   = "interflow.tfsstate"
   }
 }
